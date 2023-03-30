f23d751a3155e95656fbf43c93e5468ce4b82824
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-03-30 15:16:09 -0400

    Segunda version

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
@@ -1,7 +1,5 @@
 package com.iteneum.designsystem.components
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -10,18 +8,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview(showBackground = true)
+//recordatorio: cuando haga el push, no incluir el import de Example en el gradel de App
+//recibir HigOrderFunction = onclick, modifiers y el texto
+
+@Preview()
 @Composable
-fun LpOutlineButton(){
-    Button(onClick = {},
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(40.dp),
+fun LpOutlineButton(onClick: () -> Unit, textButtonDescription: String, modifier: Modifier){
+    Button(onClick = onClick,
+        modifier,
         shape = RoundedCornerShape(12.dp),
-        colors = ButtonDefaults.buttonColors(contentColor = Color.Black)
+        colors = ButtonDefaults.buttonColors(contentColor = Color.Black, containerColor = MaterialTheme.colorScheme.primary)
     ){
-        Text("Login", )
+        Text(textButtonDescription)
     }
 }
 
+@Composable
+fun testButton( ){
+
+}
 
--- feature/example/src/main/java/com/iteneum/example/ExampleView.kt
+++ feature/example/src/main/java/com/iteneum/example/ExampleView.kt
@@ -1,12 +1,19 @@
 package com.iteneum.example
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.iteneum.designsystem.components.LpBasicCard
+import com.iteneum.designsystem.components.LpOutlineButton
 
+var modifier = Modifier.fillMaxWidth().height(40.dp)
 @Composable
 fun ExampleView(){
-    LpBasicCard() {
-        Text("Hello")
+    LpBasicCard {
+        LpOutlineButton(onClick = {} ,"Login", modifier)
     }
 }
