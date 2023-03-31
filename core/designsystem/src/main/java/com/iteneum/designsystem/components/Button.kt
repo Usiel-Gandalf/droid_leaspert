77344cf38706997d91089244079a7876482847b2
Usiel Garcia Jimenez <usieloficial@gmail.com>
2023-03-31 10:20:40 -0400

    First version for review

--- core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
+++ core/designsystem/src/main/java/com/iteneum/designsystem/components/Button.kt
@@ -1,5 +1,7 @@
 package com.iteneum.designsystem.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
@@ -7,12 +9,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun LpFilledTonalButton(modifier: Modifier, textButtonDescription: String, onClick: () -> Unit){
-    FilledTonalButton(onClick = onClick,
-        modifier,
+fun LpFilledTonalButton(modifier: Modifier, textButtonDescription: String, onClick: () -> Unit) {
+    FilledTonalButton(
+        onClick = onClick,
+        modifier
+            .padding(10.dp)
+            .fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
-        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary, contentColor = MaterialTheme.colorScheme.secondary)
-    ){
+        colors = ButtonDefaults.buttonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.secondary
+        )
+    ) {
         Text(textButtonDescription)
     }
 }
